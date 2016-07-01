184b9c4b30a91e3e8c17c6193f935e9119a1feb7
Danial Pervaiz <codemythought@gmail.com>
2016-06-30 23:25:27 -0600

    Added Changes

--- dashboard/js/custom.js
+++ dashboard/js/custom.js
@@ -7,22 +7,42 @@
 	
 */
 
-  $('.SlideMenu').on('click', function () {
+  // $('.SlideMenu').on('click', function () {
 
-    $('.asideMenu').css({
-      'left':'-190px'
-    });
-    $('.slideMenuOpen').empty();
-    $('.slideMenuOpen').html('<i class="fa fa-arrow-circle-right openMenu" aria-hidden="true"></i>');
+  //   $('.asideMenu').css({
+  //     'left':'-190px'
+  //   });
 
-    $('.openMenu').on('click', function() {
-      $('.asideMenu').css({
-        'left':'0px'
-      });
-      $('.slideMenuOpen').empty();
-      $('.slideMenuOpen').html('<i class="fa fa-align-justify SlideMenu" aria-hidden="true"></i>');
-    });
+  //   $('.slideMenuOpen').empty();
+  //   $('.slideMenuOpen').html('<i class="fa fa-arrow-circle-right openMenu" aria-hidden="true"></i>');
 
-  });
+  // });
+
+  // $('.openMenu').on('click', function() {
+  //   $('.asideMenu').css({
+  //     'left':'0px'
+  //   });
+  //   $('.slideMenuOpen').empty();
+  //   $('.slideMenuOpen').html('<i class="fa fa-align-justify SlideMenu" aria-hidden="true"></i>');
+  // });
+
 
   
+$('.slideMenuOpen').click(function() {
+
+  if ($(this).children('i').hasClass('SlideMenu')) {
+    $('.asideMenu').css({
+      'left':'-200px'
+    });
+    $(this).empty();
+    $(this).html('<i class="fa fa-arrow-circle-right openMenu" aria-hidden="true"></i>');
+  }
+  else{
+    $('.asideMenu').css({
+      'left':'0px'
+    });
+    $(this).empty();
+    $(this).html('<i class="fa fa-align-justify SlideMenu" aria-hidden="true"></i>');
+  }
+
+});
