258f1126d6e6248deba34c3fb1409aed07cba7f4
Danial Pervaiz <codemythought@gmail.com>
2016-07-05 21:35:58 -0600

    Added Changes Auto Suggestions

--- dashboard/js/custom.js
+++ dashboard/js/custom.js
@@ -57,5 +57,27 @@
     $('.slideMenuOpen').trigger('click');
   }
 
-  /* */
+  /* auto complete form fields */
 
+ 
+  var options = {
+    data: [ {name: "Avionet", type: "air", icon: "http://lorempixel.com/100/50/transport/2"},
+      {name: "Car", type: "ground", icon: "http://lorempixel.com/100/50/transport/8"},
+      {name: "Motorbike", type: "ground", icon: "http://lorempixel.com/100/50/transport/10"},
+      {name: "Plane", type: "air", icon: "http://lorempixel.com/100/50/transport/1"},
+      {name: "Train", type: "ground", icon: "http://lorempixel.com/100/50/transport/6"}],
+
+    getValue: "name",
+
+    template: {
+      type: "description",
+      fields: {
+        description: "type"
+      }
+    }
+  };
+  $("#basics").easyAutocomplete(options);
+
+  $('.easy-autocomplete-container').on('click', function(){
+    alert('');
+  }); 
